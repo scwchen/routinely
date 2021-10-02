057c34e9df13e7befa737ec8c1f370ff8e336ae8
Steven Chen <steven.chen.121@gmail.com>
2021-10-02 18:15:02 -0400

    Added Firebase subscription and initialized SCSS files.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,51 @@ import './App.scss';
 
 function App() {
 
+  const [routineList, setRoutineList] = useState([]);
+  const [userRoutine, setUserRoutine] = useState({});
+
+  // Running this useEffect only when the component mounts
+  useEffect(() => {
+    const dbRef = ref(realtime);
+
+
+    // We grab a snapshot of our database and use the .val method to parse the JSON object that is our database data out of it
+    onValue(dbRef, (snapshot) => {
+
+      const myRoutines = snapshot.val();
+      // Have to go down one level further 
+
+      const newRoutineArray = [];
+
+      for (let propName in myRoutines) {
+        const newRoutine = {
+          key: propName,
+          routineName: myRoutines[propName]
+        };
+
+        newRoutineArray.push(newRoutine);
+      };
+
+      setRoutineList(newRoutineArray);
+
+    });
+
+  }, []);
+
   return (
     <div className="App">
       <h1>Solo React Project!</h1>
-      <p>Testing font size and SASS</p>
+      <ul>
+        {
+          routineList.map((individualRoutine) => {
+            return (
+              <li key={individualRoutine.key}>
+                <p>Test</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
