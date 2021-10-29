02fc1c4c1c22e79b1ab6c8a34a468afca856cb37
Steven Chen <steven.chen.121@gmail.com>
2021-10-29 12:42:17 -0400

    Changed footer link to portfolio instead of github.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer>
             <p>Created by
-                <a href="https://github.com/scwchen" target="_blank" rel="noreferrer"> Steven Chen </a>
+                <a href="https://stevenchen.ca" rel="noreferrer"> Steven Chen </a>
                 at
-                <a href="https://junocollege.com/" target="_blank" rel="noreferrer"> Juno College</a> 2021
+                <a href="https://junocollege.com/" rel="noreferrer"> Juno College</a> 2021
             </p>
         </footer>
     );
